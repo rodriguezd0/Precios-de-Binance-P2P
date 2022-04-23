9d08d474c73bf2aacab1d61eddc39cea57c41cc9
Daniel Sebastián Rodríguez <52185450+rodriguezd0@users.noreply.github.com>
2022-04-22 22:41:54 -0300

    Ahora funcionan otras monedas FIAT

--- precios.py
+++ precios.py
@@ -5,22 +5,22 @@ import sys
 import webbrowser
 
 def parser_error(errmsg):
-    print("\tEjemplo: \r\npython " + sys.argv[0] + " -p 3000 -m ARS -t SELL [[USA -h para sabr como usar los parametros]]")
+    print("\tEjemplo: \r\npython " + sys.argv[0] + " -p 3000 -m ARS -t SELL [[Usa -h para sabr como usar los parametros]]")
     print("Error: " + errmsg)
     sys.exit()
 
 
 def parse_args():
-    # parse the arguments
     parser = argparse.ArgumentParser(epilog='\tEjemplo: \r\npython ' + sys.argv[0] + " -p 3000 -m ARS -t SELL")
     parser.error = parser_error
     parser._optionals.title = "OPTIONS"
     parser.add_argument('-c', '--cantidad', help="Cantidad a vender", required=False, default= "0")
-    parser.add_argument('-m', '--moneda', help='Moneda que quieras obtener (Por defecto: ARS)', required=False, default="ARS")
+    parser.add_argument('-f', '--fiat', help='Moneda que quieras obtener (Por defecto: ARS)', required=False, default="ARS")
     parser.add_argument('-t', '--tipo', help='Tipo de operacion BUY/SELL (Por defecto: SELL)', required=False, default="SELL")
     return parser.parse_args()
 
-def peticion(moneda,tipo,cantidad):
+
+def peticion(fiat,tipo,cantidad):
     url = "https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search"
     headers = {
         "Accept": "*/*",
@@ -38,45 +38,59 @@ def peticion(moneda,tipo,cantidad):
     }
     data = {
         "asset": "USDT",
-        "fiat": moneda,
+        "fiat": fiat,
         "merchantCheck": True,
         "page": 1,
-        "payTypes": ["MercadoPago"],
+        "payTypes": None,
         "publisherType": None ,
         "rows": 20,
         "tradeType": tipo,
         "transAmount":  cantidad
     }
-
     r = requests.post(url, headers=headers, json=data)
     r = json.loads(r.text)
     return r
 
+
+def imprimir_metodos_de_pago(metodos):
+    todos_los_metodos = ""
+    for i in metodos:
+        todos_los_metodos += i+' '
+    return todos_los_metodos
+
+
 def procesar_peticion(r):
-    lista_de_vendedores = []
+    lista_de_comerciantes = []
     for i in r['data']:
-        vendedor = []
-        vendedor.append(i['adv']['price'])
-        vendedor.append(i['advertiser']['nickName'])
-        vendedor.append(i['advertiser']['userNo'])
-        lista_de_vendedores.append(vendedor)
+        comerciante = []
+        metodos = []
+        comerciante.append(i['adv']['price'])
+        for k in i['adv']['tradeMethods']:
+            metodos.append(k['payType'])
+        comerciante.append(metodos)
+        comerciante.append(i['advertiser']['nickName'])
+        comerciante.append(i['advertiser']['userNo'])
+        lista_de_comerciantes.append(comerciante)
 
-    print("VENDEDORES")
-    for i in range(len(lista_de_vendedores)):
-        print(f"{i+1}. El precio es de {lista_de_vendedores[i][0]}. y el vendedor es {lista_de_vendedores[i][1]}.")
+
+    print("COMERCIANTES")
+    for i in range(len(lista_de_comerciantes)):
+        print(f"{i+1}. El precio es de ${lista_de_comerciantes[i][0]}.   Comerciante: {lista_de_comerciantes[i][2]}.   Metodos de pago: {imprimir_metodos_de_pago(lista_de_comerciantes[i][1])}")
     print()
+    try:
+        eleccion = int(input("Ingresa el numero (ENTER para salir) : "))-1
+        webbrowser.open("https://p2p.binance.com/es/advertiserDetail?advertiserNo="+lista_de_comerciantes[eleccion][3], new=2, autoraise=True)
+    except:
+        return
 
-    eleccion = int(input("Ingresa el numero (0 para salir) : "))-1
-    if eleccion != -1:
-        webbrowser.open("https://p2p.binance.com/es/advertiserDetail?advertiserNo="+lista_de_vendedores[eleccion][2], new=2, autoraise=True)
 
 def ejecutar():
     argumentos = parse_args()
-    moneda = argumentos.moneda
+    fiat = argumentos.fiat
     tipo = argumentos.tipo
     cantidad = argumentos.cantidad
-    todos_los_vendedores = peticion(moneda,tipo,cantidad)
-    procesar_peticion(todos_los_vendedores)
+    todos_los_comerciantes = peticion(fiat,tipo,cantidad)
+    procesar_peticion(todos_los_comerciantes)
 
 
 if __name__ == "__main__":
